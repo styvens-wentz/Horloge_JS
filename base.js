cef80cdc0ff3b53c28611b60006b60b7c3c1e518
styvens59 <56022040+styvens59@users.noreply.github.com>
2019-12-09 20:01:55 +0100

    Horloge fini

---
+++ base.js
@@ -0,0 +1,34 @@
+const heure = document.getElementById('heure');
+const minute = document.getElementById('minute');
+const seconde = document.getElementById('seconde');
+const date_actuelle = new Date();
+
+let rotate_seconde = (date_actuelle.getSeconds() * 6);
+let rotate_minute = (date_actuelle.getMinutes() * 6);
+let rotate_heure = (date_actuelle.getHours() * 30) + (date_actuelle.getMinutes() * 0.5);
+
+
+
+
+const temps_seconde = function () {
+    setTimeout(temps_seconde, 1000);
+    if (date_actuelle.getMilliseconds() >= 0) {
+        rotate_seconde += 6;
+    }
+    if (seconde.style.transform === 'rotate(360deg)') {
+        rotate_minute += 6;
+        rotate_heure += 0.5;
+        seconde.style.transform = 'rotate(6deg)';
+    }
+    seconde.style.transform = 'rotate(' + rotate_seconde + 'deg)';
+    minute.style.transform = 'rotate(' + rotate_minute + 'deg)';
+    heure.style.transform = 'rotate(' + rotate_heure + 'deg)';
+    console.log(rotate_heure);
+};
+
+
+
+
+
+temps_seconde();
+
